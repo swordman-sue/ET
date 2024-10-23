bc0e8048210fc629d7ff1bd6361784788326f0f8
swordman sue <jy02789801@gmail.com>
2024-10-24 03:10:57 +0800

    进Editor Play Mode前拷贝最新的热更dll到Define.CodeDir

--- Packages/cn.etetet.loader/Editor/Assembly/AssemblyEditor.cs
+++ Packages/cn.etetet.loader/Editor/Assembly/AssemblyEditor.cs
@@ -35,12 +35,18 @@ namespace ET
                 string dllFile = $"{Application.dataPath}/../Library/ScriptAssemblies/{dll}.dll";
                 if (File.Exists(dllFile))
                 {
+                    /* 由于OnGenerateCSProjectProcessor.GenerateCustomProject中插入的生成后事件，对VS2022无效
+                     * 且进Editor Play Mode前，需执行ET/Loader/Compile _F6，否则无法在Define.CodeDir加载到最新的热更dll
+                     * 所以在此处加入File.Copy(拷贝最新的热更dll到Define.CodeDir)
+                     */
+                    File.Copy(dllFile, $"{Define.CodeDir}/{dll}.dll.bytes", true);
                     File.Delete(dllFile);
                 }
 
                 string pdbFile = $"{Application.dataPath}/../Library/ScriptAssemblies/{dll}.pdb";
                 if (File.Exists(pdbFile))
                 {
+                    File.Copy(pdbFile, $"{Define.CodeDir}/{dll}.pdb.bytes", true);
                     File.Delete(pdbFile);
                 }
             }
